6162d126fa3a9cdafe407d420fcae807340ce858
Sayr Olivares <78969699+sayrolivares@users.noreply.github.com>
2026-05-28 01:25:18 +0000

    Edited dockerfile name, some updates to feed.py and entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,12 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-python3 usr/bin/feed.py
+cd /github/workspace
 
-git add -A && git commit -m "Update Feed" && git push --set-upstream origin main
+python3 /usr/bin/feed.py
+
+git add -A && git commit -m "Update Feed"
+git push --set-upstream origin main
 
 
 echo "=============="
